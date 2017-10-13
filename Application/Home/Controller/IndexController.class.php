972e3a4879b0d632e22f4e6879db6f67d075ae1e
hansminner <360312085@qq.com>
2017-10-13 15:57:26 +0800

    默认

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,10 +4,14 @@ namespace Home\Controller;
 use Think\Controller;
 
 class IndexController extends Controller {
-    public function index() {
+    public function index(){
         $db = M('summary');
         $list = $db->field('system,content,contradiction,characteristics,motivation')->select();
-        $this->assign('list',$list);
+        $this->assign('list', $list);
+        $this->display();
+    }
+
+    public function aaa(){
         $this->display();
     }
 }
